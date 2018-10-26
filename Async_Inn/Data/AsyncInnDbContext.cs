6c041414d8692d1ba2976832e2a2152f115796c9
jimmyn123 <jimmyn123@gmail.com>
2018-10-25 18:11:32 -0700

    finished adding db

---
+++ Async_Inn/Data/AsyncInnDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Async_Inn.Data
+{
+    public class AsyncInnDbContext : DbContext
+    {
+        public AsyncInnDbConext(DbContextOptions<AsyncInnDbContext> options) : base(options)
+        {
+        }
+    }
+}
