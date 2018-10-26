c25e92ca1330f26406780e30daf2644608ea20d7
Jimmy Nguyen <jimmyn123@gmail.com>
2018-10-25 21:13:08 -0700

    scaffolded all of the controllers and views

--- Async_Inn/Migrations/20181026040421_createdTables.Designer.cs
+++ Async_Inn/Migrations/20181026040421_createdTables.Designer.cs
@@ -4,14 +4,16 @@ using Async_Inn.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Async_Inn.Migrations
 {
     [DbContext(typeof(AsyncInnDbContext))]
-    partial class AsyncInnDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181026040421_createdTables")]
+    partial class createdTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
