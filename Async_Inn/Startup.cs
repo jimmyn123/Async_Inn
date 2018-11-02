18f1e9ff318eb69aa264b129e3dceb4f170567ad
jimmyn123 <jimmyn123@gmail.com>
2018-11-02 15:21:31 -0700

    properly added user secrets

--- Async_Inn/Startup.cs
+++ Async_Inn/Startup.cs
@@ -20,7 +20,9 @@ namespace Async_Inn
 
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            var cb = new ConfigurationBuilder().AddEnvironmentVariables();
+            cb.AddUserSecrets<Startup>();
+            Configuration = cb.Build();
         }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
