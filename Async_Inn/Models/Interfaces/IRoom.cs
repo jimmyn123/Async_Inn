fc9b2fd9c8b0c9def4b8589d4eabf3a9ad68b023
Jimmy Nguyen <jimmyn123@gmail.com>
2018-11-02 00:38:02 -0700

    Added IRooms interface and RoomService

---
+++ Async_Inn/Models/Interfaces/IRoom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Async_Inn.Models.Interfaces
+{
+    public interface IRoom
+    {
+        Task CreateRoom(Room room);
+        Task UpdateRoom(Room room);
+        Task<Room> GetRoom(int? id);
+        Task<IEnumerable<Room>> GetRooms();
+        Task DeleteRoom(Room room);
+    }
+}
---
+++ Async_Inn/Models/Services/RoomService.cs
@@ -0,0 +1,49 @@
+﻿using Async_Inn.Data;
+using Async_Inn.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Async_Inn.Models.Services
+{
+    public class RoomService : IRoom
+    {
+        private AsyncInnDbContext _context;
+
+        public RoomService(AsyncInnDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateRoom(Room room)
+        {
+            _context.Rooms.Add(room);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteRoom(Room room)
+        {
+            _context.Rooms.Remove(room);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Room> GetRoom(int? id)
+        {
+            return await _context.Rooms.FirstOrDefaultAsync(x => x.ID == id);
+        }
+
+        public async Task<IEnumerable<Room>> GetRooms()
+        {
+            return await _context.Rooms.ToListAsync();
+        }
+
+        public async Task UpdateRoom(Room room)
+        {
+            _context.Rooms.Update(room);
+            await _context.SaveChangesAsync();
+
+        }
+    }
+}
